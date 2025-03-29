39ddb7a4d1f0b243eb17d55f0b700f1081fd9a52
MahdiLcoder <labidimahdi95@gmail.com>
2025-03-29 13:20:03 +0100

    ..

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,11 @@ import PostList from '../components/PostList'
 
 function Home() {
   return (
-    <div className="flex flex-col items-center pt-20">
+    <div className="flex flex-col  items-center pt-20">
       <h2 className="text-6xl font-extrabold mb-8 text-center bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
       Recent Posts
       </h2>
-      <div className="w-full max-w-4xl my-3">
+      <div className="w-full  my-3">
       <PostList />
       </div>
     </div>
